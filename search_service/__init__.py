8e96535e4a7d5e71d7dec3644f2fb3a4268ce670
henriBensard <besnard.hounwanou@gmail.com>
2025-06-27 20:49:19 +0200

    Ajout sync elasticsearch dans enrichment_service

--- search_service/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Service de recherche Harena.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Harena Team"
-__description__ = "Service de recherche pour transactions financières"
-
-
--- search_service/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module core contenant la logique métier du service de recherche.
-
-Inclut le moteur de recherche hybride, les services d'embeddings,
-de reranking et de traitement des requêtes.
-"""
-
-__all__ = []

88df8c199c27c24a9aaa5d2b70feda0fb331150b
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-10 23:06:21 +0200

    Use rule metadata to skip search and suggest responses

--- conversation_service/intent_rules/rule_loader.py
+++ conversation_service/intent_rules/rule_loader.py
@@ -56,6 +56,8 @@ class IntentRule:
     search_parameters: Optional[Dict] = None
     default_filters: Optional[List[Dict]] = None
     examples: Optional[List[str]] = None
+    no_search_needed: bool = False
+    suggested_responses: Optional[List[str]] = None
     
     def __post_init__(self):
         """Validation post-initialisation"""
@@ -236,7 +238,9 @@ class RuleLoader:
                 exact_matches=set(config.get('exact_matches', [])),
                 search_parameters=config.get('search_parameters'),
                 default_filters=config.get('default_filters'),
-                examples=config.get('examples')
+                examples=config.get('examples'),
+                no_search_needed=config.get('no_search_needed', False),
+                suggested_responses=config.get('suggested_responses')
             )
             
             return rule

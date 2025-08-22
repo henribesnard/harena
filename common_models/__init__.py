6c8cf1fc58174a53177404e99f776f65402f1511
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-22 19:56:55 +0200

    Add flexible service data models

---
+++ common_models/__init__.py
@@ -0,0 +1,11 @@
+from .dynamic_search import DynamicSearchServiceQuery
+from .search_response import SearchServiceResponse
+from .user_profile import UserServiceProfile
+from .cache_key import DynamicCacheKey
+
+__all__ = [
+    "DynamicSearchServiceQuery",
+    "SearchServiceResponse",
+    "UserServiceProfile",
+    "DynamicCacheKey",
+]

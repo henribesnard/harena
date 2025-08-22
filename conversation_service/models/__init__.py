6fcb1796293cb76ba324f2eaaa9d5002314fb280
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-22 19:50:27 +0200

    Add conversation enums with validation

---
+++ conversation_service/models/__init__.py
@@ -0,0 +1,10 @@
+"""Exports publics du package `conversation_service.models`."""
+
+from .enums import ConfidenceThreshold, EntityType, IntentType, QueryType
+
+__all__ = [
+    "IntentType",
+    "EntityType",
+    "QueryType",
+    "ConfidenceThreshold",
+]

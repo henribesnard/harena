d30ed6b3a96d925dd5dc5a6d494a15c4ba93992c
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-22 21:49:41 +0200

    Use Pydantic v2 validation

--- conversation_service/core/validators.py
+++ conversation_service/core/validators.py
@@ -13,15 +13,12 @@ def validate_model(model: Type[T], data: Any) -> T:
     """Validate ``data`` against ``model``.
 
     If ``data`` is already an instance of ``model`` it is returned as-is.
-    Otherwise ``model.parse_obj`` is used to construct and validate an instance.
+    Otherwise ``model.model_validate`` is used to construct and validate an instance.
     """
 
     if isinstance(data, model):
         return data
     try:
-        return model.parse_obj(data)  # Pydantic v1
-    except AttributeError:  # pragma: no cover - support for Pydantic v2
-        try:
-            return model.model_validate(data)  # type: ignore[attr-defined]
-        except AttributeError:
-            return model(**data)
+        return model.model_validate(data)  # type: ignore[attr-defined]
+    except AttributeError:  # pragma: no cover - support for Pydantic v1
+        return model(**data)

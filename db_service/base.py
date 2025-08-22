aad8b578582da4be06debaf074769143a8634f9b
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-22 13:40:34 +0200

    Use declarative_base from sqlalchemy.orm

--- db_service/base.py
+++ db_service/base.py
@@ -1,10 +1,18 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, DateTime
 from sqlalchemy.sql import func
 
+
 Base = declarative_base()
+
 
 class TimestampMixin:
     """Mixin qui ajoute des champs de timestamp à tous les modèles."""
+
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )

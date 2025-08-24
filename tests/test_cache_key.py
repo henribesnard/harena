559d98a588936d3888ffe519b4a2964a40b0a537
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-24 20:35:30 +0200

    feat(search): support highlight in requests and cache

--- tests/test_cache_key.py
+++ tests/test_cache_key.py
@@ -21,6 +21,14 @@ def test_cache_key_includes_aggregations_and_flag():
     flag_req = SearchRequest(user_id=1, query="", filters={}, aggregation_only=True)
     assert engine._generate_cache_key(flag_req) != base_key
 
+    highlight_req = SearchRequest(
+        user_id=1,
+        query="",
+        filters={},
+        highlight={"fields": {"primary_description": {}}},
+    )
+    assert engine._generate_cache_key(highlight_req) != base_key
+
 
 @pytest.mark.asyncio
 async def test_cache_get_set_use_generated_key():

8638a415d0577e8ccbd84c14d183a8c9a7f7d60f
hounwanou-b <53481221+henribesnard@users.noreply.github.com>
2025-08-28 22:42:02 +0200

    feat: add entity extraction prompts

--- conversation_service/prompts/autogen/__init__.py
+++ conversation_service/prompts/autogen/__init__.py
@@ -1,1 +1,9 @@
-"""Prompts AutoGen pour classification."""
+"""Prompts AutoGen."""
+
+from .entity_extraction_prompts import ENTITY_EXTRACTION_SYSTEM_MESSAGE
+from .intent_classification_prompts import AUTOGEN_INTENT_SYSTEM_MESSAGE
+
+__all__ = [
+    "ENTITY_EXTRACTION_SYSTEM_MESSAGE",
+    "AUTOGEN_INTENT_SYSTEM_MESSAGE",
+]
